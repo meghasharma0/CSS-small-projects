0bce22746c310dd153f1f8e99c3ae77dfbaeab59
Megha Sharma <meghaalg7@gmail.com>
2024-02-03 10:35:38 +0530

    class components and props

---
+++ learning/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Message from './class/Message';
+
+function App() {
+  return (
+    <>
+      <Message message="good morning" />
+    </>
+  );
+}
+
+export default App;
---
+++ learning/src/class/Message.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Message extends Component {
+    render(){
+        return (
+            <>
+                <h1>Class {this.props.message}</h1>
+                <h1>hiii</h1>
+            </>
+        )
+    }
+}
